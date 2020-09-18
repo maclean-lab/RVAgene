9b2aae338ce16a4b0a7a54c0d90623b1f10d03eb
timkartar <timkartar7879@gmail.com>
2020-09-17 23:33:16 -0700

    remove extra file

--- rvagene/base.py
+++
@@ -1,8 +0,0 @@
-from sklearn.base import BaseEstimator as SklearnBaseEstimator
-
-
-class BaseEstimator(SklearnBaseEstimator):
-    # http://msmbuilder.org/development/apipatterns.html
-
-    def summarize(self):
-        return 'NotImplemented'
